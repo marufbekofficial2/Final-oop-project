83ff0923b174201bc2c090b5bf55efd79f4e5526
Maruf Mamatqulov <marufbekofficial2@gmail.com>
2023-12-19 09:41:32 +0500

    doc files

---
+++ doc/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"org.example","c":"CourseProject","l":"CourseProject()","u":"%3Cinit%3E()"},{"p":"org.example","c":"CourseProject","l":"main(String[])","u":"main(java.lang.String[])"},{"p":"org.example","c":"CourseProject","l":"printCategories()"},{"p":"org.example","c":"CourseProject","l":"printRecords()"}];updateSearchResults();
